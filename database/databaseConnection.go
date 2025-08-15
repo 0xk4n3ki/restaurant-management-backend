74566210201ec156251688aeb583308a2c4c21ea
0xk4n3ki <k4n3ki.ctf@gmail.com>
2025-08-15 19:16:25 +0530

    first commit

---
+++ database/databaseConnection.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	MongoDb := "mongo://localhost:27017"
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(MongoDb))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("connected to MongoDB")
+
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	collection := client.Database("restaurant").Collection(collectionName)
+
+	return collection
+}
